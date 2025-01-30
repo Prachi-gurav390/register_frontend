b82e0075d26e06b7a091677a3d50648ef248caf9
Prachi3045 <pikugurav007@gmail.com>
2025-01-30 12:20:21 +0530

    Updated config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    apiUrl: process.env.NODE_ENV === 'production'
-      ? 'https://your-backend-domain.vercel.app'
-      : 'http://localhost:5000'
-  };
-  
-  export default config;
+  apiUrl: process.env.NODE_ENV === 'production'
+    ? 'https://register-backend-chi.vercel.app/'
+    : 'http://localhost:5000'
+};
+
+export default config;
